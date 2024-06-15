1675d8c02dc55e96400817f8195ef9f3e025d2a1
Nishant Shukla <92618458+nsshukla23@users.noreply.github.com>
2024-06-15 16:23:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from keras.models import load_model
 import streamlit as st
 import matplotlib.pyplot as plt
 
-model = load_model(r'D:\smpPy\StockPredictionsModel.keras')
+model = load_model(r'StockPredictionsModel.keras')
 
 st.header('Stock Market Predictor')
 
